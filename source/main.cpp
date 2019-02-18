862e666afeaf321046d02d01f0dbbe40b853463e
temportalflux <dustin.yost.t@gmail.com>
2019-02-17 23:55:55 -0500

    Add basic GLFW window

--- source/main.cpp
+++ source/main.cpp
@@ -4,14 +4,52 @@
 
 using namespace std;
 
+#define LogEngine "TemportalEngine"
+
+void error_callback(int error, const char* description)
+{
+	logging::log(LogEngine, logging::ECategory::ERROR, description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 int main()
 {
-	logging::log("TemportalEngine", logging::ECategory::INFO, "Hello World!");
+	logging::log(LogEngine, logging::ECategory::INFO, "Hello World!");
 
+	glfwSetErrorCallback(error_callback);
 	if (!glfwInit())
 	{
-		logging::log("TemportalEngine", logging::ECategory::INFO, "Failed to initialize GLFW");
+		logging::log(LogEngine, logging::ECategory::ERROR, "Failed to initialize GLFW");
+		return 1;
 	}
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Temportal Engine", nullptr, nullptr);
+	if (!window)
+	{
+		// Window or OpenGL context creation failed
+		logging::log(LogEngine, logging::ECategory::ERROR, "Failed to create GLFW window");
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+
+	glfwMakeContextCurrent(window);
+	//gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+	glfwSwapInterval(1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
 	glfwTerminate();
 
 	return 0;
