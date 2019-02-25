e9cab2d87cfa4a1dae4a0ff5d5f9a2655444d7f1
temportalflux <dustin.yost.t@gmail.com>
2019-02-24 23:07:09 -0500

    Expose more math for quaternions
    Inspired/informed by Dan Buckstein

--- TemportalEngine/include/math/Vector.hpp
+++ TemportalEngine/include/math/Vector.hpp
@@ -10,6 +10,7 @@
 #include <type_traits>
 #include <cmath>
 #include <cassert>
+#include <algorithm>
 
 NS_MATH
 
@@ -45,15 +46,27 @@ public:
 		memset(mValues, 0, TValueCount);
 	}
 
-	template <typename ... T>
-	Vector(T... values)
-		: mValues{ (TValue)values... }
+	Vector(std::initializer_list<TValue> values)
 	{
+		std::copy(values.begin(), values.end(), mValues);
 	}
 
-	Vector(Vector<TValue, TValueCount> const &other)
-		: Vector(other.mValues)
-	{ }
+	Vector(Self const &other)
+	{
+		memcpy_s(mValues, TValueCount, other.mValues, TValueCount);
+	}
+
+	template <ui32 TValueCountOther>
+	Vector(Vector<TValue, TValueCountOther> const &other)
+	{
+		memset(mValues, 0, TValueCount);
+		other.getValues(mValues);
+	}
+
+	void getValues(TValue out[TValueCount]) const
+	{
+		memcpy_s(out, TValueCount, mValues, TValueCount);
+	}
 
 	TValue& operator[](ui32 const i)
 	{
@@ -66,6 +79,70 @@ public:
 		assert(i < TValueCount);
 		return mValues[i];
 	}
+
+	// ------------------------------------------------------------------------
+	// Properties
+
+	TValue const x() const
+	{
+		static_assert(TValueCount >= 1, "Cannot get X component of vector size < 1");
+		return mValues[0];
+	}
+
+	TValue const x(TValue const &value)
+	{
+		static_assert(TValueCount >= 1, "Cannot get X component of vector size < 1");
+		mValues[0] = value;
+		return mValues[0];
+	}
+
+	TValue const y() const
+	{
+		static_assert(TValueCount >= 2, "Cannot get Y component of vector size < 2");
+		return mValues[1];
+	}
+
+	TValue const y(TValue const &value)
+	{
+		static_assert(TValueCount >= 2, "Cannot get Y component of vector size < 2");
+		mValues[1] = value;
+		return mValues[1];
+	}
+
+	TValue const z() const
+	{
+		static_assert(TValueCount >= 3, "Cannot get Z component of vector size < 3");
+		return mValues[2];
+	}
+
+	TValue const z(TValue const &value)
+	{
+		static_assert(TValueCount >= 3, "Cannot get Z component of vector size < 3");
+		mValues[2] = value;
+		return mValues[2];
+	}
+
+	TValue const w() const
+	{
+		static_assert(TValueCount >= 4, "Cannot get W component of vector size < 4");
+		return mValues[3];
+	}
+
+	TValue const w(TValue const &value)
+	{
+		static_assert(TValueCount >= 4, "Cannot get W component of vector size < 4");
+		mValues[3] = value;
+		return mValues[3];
+	}
+
+	template <ui8 TSubCount>
+	Vector<TValue, TSubCount> const subvector() const
+	{
+		return Vector<TValue, TSubCount>(*this);
+	}
+
+	// ------------------------------------------------------------------------
+	// Generic Properties
 
 	static TValue const dot(Self const &a, Self const &b)
 	{
@@ -135,6 +212,29 @@ public:
 			mValues[i] *= other.mValues[i];
 	}
 
+	Self operator*(TValue const other) const
+	{
+		Self ret = *this;
+		//ret *= other;
+		return ret;
+	}
+
+	void operator*=(TValue const other)
+	{
+		for (ui8 i = 0; i < TValueCount; ++i)
+			mValues[i] *= other;
+	}
+
+	friend Self operator*(TValue const scalar, Self const vector)
+	{
+		return vector * scalar;
+	}
+
+	Self operator-() const
+	{
+		return inverse();
+	}
+
 	Self inverse() const
 	{
 		Self ret = Self(*this);
@@ -169,6 +269,7 @@ public:
 typedef Vector<f32, 2> Vector2;
 typedef Vector<f32, 3> Vector3;
 typedef Vector<f32, 4> Vector4;
+typedef Vector4 Quaternion;
 
 typedef Vector<i32, 2> Vector2Int;
 typedef Vector<i32, 3> Vector3Int;
@@ -179,6 +280,18 @@ typedef Vector<ui32, 3> Vector3UInt;
 Vector2 const Vector2unitX, Vector2unitY;
 Vector3 const Vector3unitX, Vector3unitY, Vector3unitZ;
 Vector4 const Vector4unitX, Vector4unitY, Vector4unitZ, Vector4unitW;
+Quaternion const QuaternionIdentity;
+
+Quaternion const QuaternionFromAxisAngle(Vector3 const axis, float const angleRad);
+Quaternion const QuaternionConjugate(Quaternion const quat);
+Quaternion const QuaternionInverse(Quaternion const quat);
+Quaternion const QuaternionConcatenate(Quaternion const a, Quaternion const b);
+Vector3 const RotateVector(Vector3 const vector, Quaternion const rotation);
+Quaternion const MultiplyVector(Vector3 const vector, Quaternion const quat);
+
+// TODO: This should really be in a physics thing, not a math thing
+Quaternion const IntegrateKinematic(Quaternion const rotation,
+	Vector3 const angularVelocity, Vector3 const angularAcceleration, f32 const deltaTime);
 
 NS_END
 
--- TemportalEngine/source/math/Vector.cpp
+++ TemportalEngine/source/math/Vector.cpp
@@ -1,22 +1,97 @@
 #include "math/Vector.hpp"
 
+NS_MATH
+
+Quaternion const QuaternionFromAxisAngle(Vector3 const axis, float const angleRad)
+{
+	auto halfAngle = angleRad * 0.5f;
+	return Quaternion(axis * std::sin(halfAngle))
+		+ QuaternionIdentity * std::cos(halfAngle);
+}
+
+Quaternion const QuaternionConjugate(Quaternion const quat)
+{
+	// <-x, -y, -z, w>
+	return Quaternion(-quat.subvector<3>())
+		+ QuaternionIdentity * quat.w();
+}
+
+Quaternion const QuaternionInverse(Quaternion const quat)
+{
+	return QuaternionConjugate(quat).normalized();
+}
+
+Quaternion const QuaternionConcatenate(Quaternion const left, Quaternion const right)
+{
+	Vector3 lVec = left.subvector<3>();
+	Vector3 rVec = right.subvector<3>();
+	float w = left.w() * right.w() - Vector3::dot(lVec, rVec);
+	Vector3 vec = lVec * w + rVec * w + Vector3::cross(lVec, rVec);
+	return Quaternion(vec) + QuaternionIdentity * w;
+}
+
+Vector3 const RotateVector(Vector3 const vector, Quaternion const rotation)
+{
+	// vPrime = rot * v * rotConjugate
+	// OR
+	// r = quaternion vector
+	// vPrime = v + 2r x (r x v + wv)
+	return vector + Vector3::cross(
+		rotation.subvector<3>() * 2,
+		Vector3::cross(
+			rotation.subvector<3>(), vector
+		) * rotation.w() * vector
+	);
+}
+
+Quaternion const MultiplyVector(Vector3 const vector, Quaternion const quat)
+{
+	// r = quat vector3
+	// q* = < vxr + wv, -dot(v, r) >
+	return Quaternion(
+		Vector3::cross(vector, quat.subvector<3>())
+		+ vector * quat.w()
+	) + QuaternionIdentity * -Vector3::dot(vector, quat.subvector<3>());
+}
+
+Quaternion const IntegrateKinematic(Quaternion const rotation,
+	Vector3 const angularVelocity, Vector3 const angularAcceleration, f32 const deltaTime)
+{
+	// qPrime = q + (dq/dt)dt + (1/2)(d^2q/dt^2)dt^2
+	// qPrime = q + [(1/2)wdt * q] + [(1/2)aq - (1/4)|w|^2*q]dt^2
+	Quaternion integrated = rotation;
+	// integrate velocity
+	integrated += MultiplyVector(0.5f * angularVelocity * deltaTime, rotation);
+	// integrate acceleration
+	integrated += (0.5f * deltaTime * deltaTime) * (
+		MultiplyVector(0.5f * angularAcceleration, rotation)
+		-
+		(0.25f * angularVelocity.magnitudeSq() * rotation)
+	);
+	// normalize
+	return integrated.normalized();
+}
+
+NS_END
+
 using namespace math;
 
-Vector2 const Vector2::zero = Vector2(0, 0);
-Vector2 const Vector2unitX = Vector2(1, 0);
-Vector2 const Vector2unitY = Vector2(0, 1);
-Vector3 const Vector3::zero = Vector3(0, 0, 0);
-Vector3 const Vector3unitX = Vector3(1, 0, 0);
-Vector3 const Vector3unitY = Vector3(0, 1, 0);
-Vector3 const Vector3unitZ = Vector3(0, 0, 1);
-Vector4 const Vector4::zero = Vector4(0, 0, 0, 0);
-Vector4 const Vector4unitX = Vector4(1, 0, 0, 0);
-Vector4 const Vector4unitY = Vector4(0, 1, 0, 0);
-Vector4 const Vector4unitZ = Vector4(0, 0, 1, 0);
-Vector4 const Vector4unitW = Vector4(0, 0, 0, 1);
+Vector2 const Vector2::zero = Vector2{ 0, 0 };
+Vector2 const Vector2unitX = Vector2{ 1, 0 };
+Vector2 const Vector2unitY = Vector2{ 0, 1 };
+Vector3 const Vector3::zero = Vector3{ 0, 0, 0 };
+Vector3 const Vector3unitX = Vector3{ 1, 0, 0 };
+Vector3 const Vector3unitY = Vector3{ 0, 1, 0 };
+Vector3 const Vector3unitZ = Vector3{ 0, 0, 1 };
+Vector4 const Vector4::zero = Vector4{ 0, 0, 0, 0 };
+Vector4 const Vector4unitX = Vector4{ 1, 0, 0, 0 };
+Vector4 const Vector4unitY = Vector4{ 0, 1, 0, 0 };
+Vector4 const Vector4unitZ = Vector4{ 0, 0, 1, 0 };
+Vector4 const Vector4unitW = Vector4{ 0, 0, 0, 1 };
+Quaternion const QuaternionIdentity = math::Vector4unitW;
 
-Vector2Int const Vector2Int::zero = Vector2Int(0, 0);
-Vector3Int const Vector3Int::zero = Vector3Int(0, 0, 0);
+Vector2Int const Vector2Int::zero = Vector2Int{ 0, 0 };
+Vector3Int const Vector3Int::zero = Vector3Int{ 0, 0, 0 };
 
-Vector2UInt const Vector2UInt::zero = Vector2UInt(0, 0);
-Vector3UInt const Vector3UInt::zero = Vector3UInt(0, 0, 0);
+Vector2UInt const Vector2UInt::zero = Vector2UInt{ 0, 0 };
+Vector3UInt const Vector3UInt::zero = Vector3UInt{ 0, 0, 0 };
