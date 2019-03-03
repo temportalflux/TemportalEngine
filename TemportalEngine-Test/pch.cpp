9ba592ae3de2dbbe782b9262947d5f753a623d6d
temportalflux <dustin.yost.t@gmail.com>
2019-03-02 19:43:21 -0500

    Wipe test suite

--- TemportalEngine-Test/pch.cpp
+++ TemportalEngine-Test/pch.cpp
@@ -1,1 +1,6 @@
+//
+// pch.cpp
+// Include the standard header and generate the precompiled header.
+//
+
 #include "pch.h"
--- TemportalEngine-Test/pch.h
+++ TemportalEngine-Test/pch.h
@@ -1,2 +1,8 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
 #pragma once
+
 #include "gtest/gtest.h"
