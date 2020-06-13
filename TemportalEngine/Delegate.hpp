72e1584ecc88ff306ddeb2e7d5c096dee50f36e8
temportalflux <dustin.yost.t@gmail.com>
2020-06-13 09:59:02 -0400

    Update delegate to use weak ptrs as keys

--- TemportalEngine/Delegate.hpp
+++ TemportalEngine/Delegate.hpp
@@ -37,33 +37,40 @@ template <class TSignature>
 class BroadcastDelegate
 {
 	typedef std::function<TSignature> TBinding;
-	typedef void* TKey;
+	typedef std::weak_ptr<void> TKey;
+
+	struct Pair
+	{
+		TKey key;
+		TBinding binding;
+	};
 
 public:
-
-	bool hasBindingFor(TKey key)
-	{
-		return this->mBindings.find(key) != this->mBindings.end();
-	}
-
+	BroadcastDelegate() = default;
+	
 	void bind(TKey key, TBinding binding)
 	{
 		assert(!this->hasBindingFor(key));
-		this->mBindings.insert(std::make_pair(key, binding));
+		this->mBindings.push_back({ key, binding });
 	}
 
 	void unbind(TKey key)
 	{
 		assert(this->hasBindingFor(key));
-		this->mBindings.erase(this->mBindings.find(key));
+		this->mBindings.erase(this->find(key));
 	}
 
+	// 2O(n) to remove all bindings which are expired and then execute all non-expired bindings
 	template <typename... TArgs>
 	void broadcast(TArgs... args)
 	{
-		for (auto& [key, binding] : this->mBindings)
+		this->mBindings.erase(std::remove_if(
+			this->mBindings.begin(), this->mBindings.end(),
+			[](Pair const &pair) { return pair.key.expired(); }
+		), this->mBindings.end());
+		for (auto& pair : this->mBindings)
 		{
-			binding(args...);
+			pair.binding(args...);
 		}
 	}
 
@@ -71,6 +78,26 @@ private:
 	/**
 	 * A map keyed by object pointers to the binding they have added.
 	 */
-	std::unordered_map<TKey, TBinding> mBindings;
+	std::vector<Pair> mBindings;
+
+	// O(n) search for a pair with the key
+	auto find(TKey key)
+	{
+		for (auto it = this->mBindings.begin(); it != this->mBindings.end(); ++it)
+		{
+			// The keys are equivalent if neither is "before" (i.e. less than) the other
+			if (!it->key.owner_before(key) && !key.owner_before(it->key))
+			{
+				return it;
+			}
+		}
+		return this->mBindings.end();
+	}
+
+	// O(n) search for a pair with the key
+	bool hasBindingFor(TKey key)
+	{
+		return this->find(key) != this->mBindings.end();
+	}
 
 };
