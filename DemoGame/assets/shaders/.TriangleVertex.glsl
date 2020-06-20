ceacb29d40d7d8ecea8a3d64268b409fd5533e7e
temportalflux <dustin.yost.t@gmail.com>
2020-06-20 15:23:32 -0400

    Add cube model

--- DemoGame/assets/shaders/.TriangleVertex.glsl
+++ DemoGame/assets/shaders/.TriangleVertex.glsl
@@ -6,7 +6,7 @@ layout(binding = 0) uniform ModelViewProjection {
 	mat4 proj;
 } mvp;
 
-layout(location = 0) in vec2 position;
+layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
 layout(location = 2) in mat4 modelMatrix; // slots [2,5]
 
@@ -14,6 +14,6 @@ layout(location = 0) out vec3 fragColor;
 
 void main()
 {
-	gl_Position = mvp.proj * mvp.view * modelMatrix * vec4(position, 0.0, 1.0);
+	gl_Position = mvp.proj * mvp.view * modelMatrix * vec4(position, 1.0);
 	fragColor = color;
 }
--- TemportalEngine/include/ModelCube.hpp
+++ TemportalEngine/include/ModelCube.hpp
@@ -8,17 +8,58 @@ class ModelCube : public Model
 public:
 	ModelCube() : Model()
 	{
-		// TODO: The class says cube, but this is clearly a plane... :P
-		auto idxTL = this->pushVertex({ {-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f} });
-		auto idxTR = this->pushVertex({ {+0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f} });
-		auto idxBR = this->pushVertex({ {+0.5f, +0.5f, 0.0f}, {1.0f, 0.0f, 0.0f} });
-		auto idxBL = this->pushVertex({ {-0.5f, +0.5f, 0.0f}, {0.0f, 0.0f, 1.0f} });
-		this->pushIndex(idxTL);
-		this->pushIndex(idxTR);
-		this->pushIndex(idxBR);
-		this->pushIndex(idxBR);
-		this->pushIndex(idxBL);
-		this->pushIndex(idxTL);
+		// <x, y, z> = <Right/Left, Forward/Backward, Up/Down>
+		auto idxFLU = this->pushVertex({ {-0.5f, +0.5f, 1.0f}, {1.0f, 0.0f, 1.0f} });
+		auto idxFRU = this->pushVertex({ {+0.5f, +0.5f, 1.0f}, {1.0f, 1.0f, 1.0f} });
+		auto idxBRU = this->pushVertex({ {+0.5f, -0.5f, 1.0f}, {0.0f, 1.0f, 1.0f} });
+		auto idxBLU = this->pushVertex({ {-0.5f, -0.5f, 1.0f}, {0.0f, 0.0f, 1.0f} });
+		auto idxFLD = this->pushVertex({ {-0.5f, +0.5f, 0.0f}, {1.0f, 0.0f, 0.0f} });
+		auto idxFRD = this->pushVertex({ {+0.5f, +0.5f, 0.0f}, {1.0f, 1.0f, 0.0f} });
+		auto idxBRD = this->pushVertex({ {+0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f} });
+		auto idxBLD = this->pushVertex({ {-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 0.0f} });
+		// +X Right
+		this->pushIndex(idxFRD);
+		this->pushIndex(idxFRU);
+		this->pushIndex(idxBRD);
+		this->pushIndex(idxBRD);
+		this->pushIndex(idxFRU);
+		this->pushIndex(idxBRU);
+		// -X Left
+		this->pushIndex(idxFLU);
+		this->pushIndex(idxFRD);
+		this->pushIndex(idxBLU);
+		this->pushIndex(idxBLU);
+		this->pushIndex(idxFRD);
+		this->pushIndex(idxBRD);
+		// +Y Forward
+		this->pushIndex(idxFRD);
+		this->pushIndex(idxFLD);
+		this->pushIndex(idxFRU);
+		this->pushIndex(idxFRU);
+		this->pushIndex(idxFLD);
+		this->pushIndex(idxFLU);
+		// -Y Backward
+		this->pushIndex(idxBRU);
+		this->pushIndex(idxBLU);
+		this->pushIndex(idxBRD);
+		this->pushIndex(idxBRD);
+		this->pushIndex(idxBLU);
+		this->pushIndex(idxBLD);
+		// +Z Up
+		this->pushIndex(idxFRU);
+		this->pushIndex(idxFLU);
+		this->pushIndex(idxBRU);
+		this->pushIndex(idxBRU);
+		this->pushIndex(idxFLU);
+		this->pushIndex(idxBLU);
+		// -Z Down
+		this->pushIndex(idxBRD);
+		this->pushIndex(idxBLD);
+		this->pushIndex(idxFRD);
+		this->pushIndex(idxFRD);
+		this->pushIndex(idxBLD);
+		this->pushIndex(idxFLD);
+
 	}
 
 };
