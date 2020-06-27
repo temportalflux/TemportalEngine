c0dacc7f067ad61109f116769874e1f5fec89fd3
temportalflux <dustin.yost.t@gmail.com>
2020-06-27 17:46:38 -0400

    Add project auto-loading for DemoGameEditor

--- DemoGameEditor/source/main.cpp
+++ DemoGameEditor/source/main.cpp
@@ -1,6 +1,8 @@
 #include "GameEditor.hpp"
 #include "Engine.hpp"
 #include "asset/AssetManager.hpp"
+#include "asset/TypedAssetPath.hpp"
+#include "asset/Project.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +19,17 @@ int main(int argc, char *argv[])
 		// Setup relevant systems in the editor (rendering, asset editors, build tasks, etc)
 		if (editor.setup())
 		{
+			// Attempt to auto-load with where we think the project file is
+			{
+				auto projPath = asset::TypedAssetPath<asset::Project>::Create(
+					std::filesystem::relative("../../DemoGame/DemoGame.te-project", std::filesystem::current_path())
+				);
+				if (projPath.path().isValid())
+				{
+					editor.setProject(projPath.load(asset::EAssetSerialization::Json));
+				}
+			}
+
 			// Execute whichever editor set needs to run. by default this will open the editor window and run until closed,
 			// but this could also just run a process like the build pipeline if the engine was configured with the correct arguments.
 			editor.run();
