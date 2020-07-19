db495f7cfcf02d77079a602f605ab88b659eb889
temportalflux <dustin.yost.t@gmail.com>
2020-07-19 08:06:29 -0400

    Fix cube model faces

--- Minecraft/include/model/ModelCube.hpp
+++ Minecraft/include/model/ModelCube.hpp
@@ -8,6 +8,6 @@ public:
 	ModelCube();
 
 private:
-	void pushFace(math::Vector3 uNeg, math::Vector3 uPos, math::Vector3 vNeg, math::Vector3 vPos);
+	void pushFace(math::Vector3 uNeg, math::Vector3 uPos, math::Vector3 vNeg, math::Vector3 vPo, math::Vector3 axis);
 
 };
--- Minecraft/source/model/ModelCube.cpp
+++ Minecraft/source/model/ModelCube.cpp
@@ -3,70 +3,50 @@
 ModelCube::ModelCube() : Model()
 {
 	// <x, y, z> = <Right/Left, Forward/Backward, Up/Down>
+	// Right +X	<U, V> = <-Y, Z>
+	this->pushFace(
+		/*U = Y 0->1*/{ 0, 0, 0 }, { 0, 1, 0 },
+		/*V = Z 0->1*/{ 0, 0, 0 }, { 0, 0, 1 },
+		/*Offset X 1*/{ 1, 0, 0 }
+	);
+	// Left -X	<U, V> = <+Y, Z>
+	this->pushFace(
+		/*U = Y 1->0*/{ 0, 1, 0 }, { 0, 0, 0 },
+		/*V = Z 0->1*/{ 0, 0, 0 }, { 0, 0, 1 },
+		/*Offset X 0*/{ 0, 0, 0 }
+	);
 	// Forward +Y	<U, V> = <-X, Z>
-	this->pushFace({ 0, 0, 0 }, { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 1 });
+	this->pushFace(
+		/*U = X 0->1*/{ 0, 0, 0 }, { 1, 0, 0 },
+		/*V = Z 0->1*/{ 0, 0, 0 }, { 0, 0, 1 },
+		/*Offset Y 0*/{ 0, 0, 0 }
+	);
 	// Backward -Y	<U, V> = <+X, Z>
-	this->pushFace({ 1, 1, 0 }, { 0, 1, 0 }, { 0, 1, 0 }, { 0, 1, 1 });
-	/*
-	auto idxFLU = this->pushVertex({ {-0.5f, +0.5f, 0.0f}, {1.0f, 0.0f, 1.0f}, { 0.0f, 0.0f} });
-	auto idxFRU = this->pushVertex({ {+0.5f, +0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, { 1.0f, 0.0f} });
-	auto idxBRU = this->pushVertex({ {+0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 1.0f}, { 1.0f, 1.0f} });
-	auto idxBLU = this->pushVertex({ {-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, { 0.0f, 1.0f} });
-	auto idxFLD = this->pushVertex({ {-0.5f, +0.5f, 1.0f}, {1.0f, 0.0f, 0.0f}, { 0.0f, 0.0f} });
-	auto idxFRD = this->pushVertex({ {+0.5f, +0.5f, 1.0f}, {1.0f, 1.0f, 0.0f}, { 1.0f, 0.0f} });
-	auto idxBRD = this->pushVertex({ {+0.5f, -0.5f, 1.0f}, {0.0f, 1.0f, 0.0f}, { 1.0f, 1.0f} });
-	auto idxBLD = this->pushVertex({ {-0.5f, -0.5f, 1.0f}, {0.0f, 0.0f, 0.0f}, { 0.0f, 1.0f} });
-	// +X Right
-	this->pushIndex(idxFRU);
-	this->pushIndex(idxFRD);
-	this->pushIndex(idxBRD);
-	this->pushIndex(idxBRD);
-	this->pushIndex(idxBRU);
-	this->pushIndex(idxFRU);
-	// -X Left
-	this->pushIndex(idxFLD);
-	this->pushIndex(idxFLU);
-	this->pushIndex(idxBLU);
-	this->pushIndex(idxBLU);
-	this->pushIndex(idxBLD);
-	this->pushIndex(idxFLD);
-	// +Y Forward
-	this->pushIndex(idxFLD);
-	this->pushIndex(idxFRD);
-	this->pushIndex(idxFRU);
-	this->pushIndex(idxFRU);
-	this->pushIndex(idxFLU);
-	this->pushIndex(idxFLD);
-	// -Y Backward
-	this->pushIndex(idxBLU);
-	this->pushIndex(idxBRU);
-	this->pushIndex(idxBRD);
-	this->pushIndex(idxBRD);
-	this->pushIndex(idxBLD);
-	this->pushIndex(idxBLU);
-	// +Z Up
-	this->pushIndex(idxFLU);
-	this->pushIndex(idxFRU);
-	this->pushIndex(idxBRU);
-	this->pushIndex(idxBRU);
-	this->pushIndex(idxBLU);
-	this->pushIndex(idxFLU);
-	// -Z Down
-	this->pushIndex(idxBLD);
-	this->pushIndex(idxBRD);
-	this->pushIndex(idxFRD);
-	this->pushIndex(idxFRD);
-	this->pushIndex(idxFLD);
-	this->pushIndex(idxBLD);
-	//*/
+	this->pushFace(
+		/*U = X 1->0*/{ 1, 0, 0 }, { 0, 0, 0 },
+		/*V = Z 0->1*/{ 0, 0, 0 }, { 0, 0, 1 },
+		/*Offset Y 1*/{ 0, 1, 0 }
+	);
+	// Up +Z	<U, V> = <-X, Y>
+	this->pushFace(
+		/*U = X 0->1*/{ 0, 0, 0 }, { 1, 0, 0 },
+		/*V = Y 0->1*/{ 0, 0, 0 }, { 0, 1, 0 },
+		/*Offset Z 1*/{ 0, 0, 1 }
+	);
+	// Down -Z	<U, V> = <+X, Y>
+	this->pushFace(
+		/*U = X 1->0*/{ 1, 0, 0 }, { 0, 0, 0 },
+		/*V = Y 0->1*/{ 0, 0, 0 }, { 0, 1, 0 },
+		/*Offset Z 0*/{ 0, 0, 0 }
+	);
 }
 
-void ModelCube::pushFace(math::Vector3 uNeg, math::Vector3 uPos, math::Vector3 vNeg, math::Vector3 vPos)
+void ModelCube::pushFace(math::Vector3 uNeg, math::Vector3 uPos, math::Vector3 vNeg, math::Vector3 vPos, math::Vector3 axis)
 {
-	auto idxTL = this->pushVertex(uNeg + vNeg, { 0.0f, 0.0f });
-	auto idxTR = this->pushVertex(uPos + vNeg, { 1.0f, 0.0f });
-	auto idxBL = this->pushVertex(uNeg + vPos, { 0.0f, 1.0f });
-	auto idxBR = this->pushVertex(uPos + vPos, { 1.0f, 1.0f });
+	auto idxTL = this->pushVertex(uNeg + vNeg + axis, { 0.0f, 0.0f });
+	auto idxTR = this->pushVertex(uPos + vNeg + axis, { 1.0f, 0.0f });
+	auto idxBL = this->pushVertex(uNeg + vPos + axis, { 0.0f, 1.0f });
+	auto idxBR = this->pushVertex(uPos + vPos + axis, { 1.0f, 1.0f });
 	this->pushIndex(idxTL);
 	this->pushIndex(idxTR);
 	this->pushIndex(idxBR);
