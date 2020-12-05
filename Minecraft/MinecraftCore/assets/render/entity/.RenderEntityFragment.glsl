e03502393687a4ad5803ae2e76f5bb60fc3b29aa
temportalflux <dustin.yost.t@gmail.com>
2020-12-05 18:26:16 -0500

    Add preliminary render pipeline assets for entities

---
+++ Minecraft/MinecraftCore/assets/render/entity/.RenderEntityFragment.glsl
@@ -0,0 +1,16 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(location = 0) in vec4 fragColor;
+layout(location = 1) in vec2 fragTexCoord;
+
+// BlockType-based unform - bound based on which block type is being drawn
+layout(set = 1, binding = 0) uniform sampler2D texSampler;
+
+layout(location = 0) out vec4 outColor;
+
+void main()
+{
+	// Actual
+	outColor = fragColor * texture(texSampler, fragTexCoord);
+}
---
+++ Minecraft/MinecraftCore/assets/render/entity/.RenderEntityVertex.glsl
@@ -0,0 +1,32 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+// Camera-based unform - changes each frame based on the camera's POV and chunk data
+layout(set = 0, binding = 0) uniform CameraUniform {
+	mat4 view;
+	mat4 proj;
+	vec3 posOfCurrentChunk;
+	vec3 chunkSize;
+} camera;
+
+// Model attributes - changes based on the block type being drawn
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec3 tangent;
+layout(location = 3) in vec3 bitangent;
+layout(location = 4) in vec2 texCoord;
+
+// Instance attributes - changes based on a specific block being drawn
+layout(location = 5) in vec3 posOfCurrentChunk;
+layout(location = 6) in mat4 modelMatrix; // slots [6,10)
+
+layout(location = 0) out vec4 fragColor;
+layout(location = 1) out vec2 fragTexCoord;
+
+void main()
+{
+	vec3 chunkPosOffset = (posOfCurrentChunk - camera.posOfCurrentChunk) * camera.chunkSize; // component-wise multiple to convert chunk pos to world pos
+	gl_Position = camera.proj * camera.view * modelMatrix * vec4(chunkPosOffset + position, 1.0);
+	fragColor = vec4(1);
+	fragTexCoord = texCoord;
+}
