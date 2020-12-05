8f5993940e2cdf5c9738cf2f0728bd7a3dfe2089
temportalflux <dustin.yost.t@gmail.com>
2020-12-05 15:35:42 -0500

    Remove tinyobjloader

--- Core/TemportalEngineEditor/source/build/asset/BuildModel.cpp
+++ Core/TemportalEngineEditor/source/build/asset/BuildModel.cpp
@@ -4,9 +4,6 @@
 #include "math/Matrix.hpp"
 #include "utility/StringUtils.hpp"
 
-#define TINYOBJLOADER_IMPLEMENTATION
-#include <tinyobjloader/tiny_obj_loader.h>
-
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
@@ -36,63 +33,6 @@ public:
 	bool detatchStream(Assimp::LogStream *pStream, unsigned int severity) override { assert(false); return false; }
 };
 
-bool loadObjModel(
-	std::filesystem::path const& objPath,
-	std::function<void(logging::ECategory const&, std::string const&)> log,
-	std::vector<asset::Model::Vertex> &outVertices, std::vector<ui32> &outIndices,
-	std::vector<std::string> &outErrors
-)
-{
-	tinyobj::attrib_t attrib;
-	std::vector<tinyobj::shape_t> shapes;
-	std::vector<tinyobj::material_t> materials;
-	std::string warning, error;
-
-	if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warning, &error, objPath.string().c_str()))
-	{
-		if (warning.length() > 0) log(LOG_WARN, warning);
-		if (error.length() > 0) log(LOG_ERR, error);
-		outErrors.push_back(error);
-		return false;
-	}
-
-	uSize uniqueVertexCount = 0;
-	for (auto const& shape : shapes)
-	{
-		for (auto const& index : shape.mesh.indices)
-		{
-			asset::Model::Vertex vertex = {};
-			vertex.position = {
-				attrib.vertices[index.vertex_index * 3 + 0],
-				attrib.vertices[index.vertex_index * 3 + 1],
-				attrib.vertices[index.vertex_index * 3 + 2]
-			};
-			vertex.texCoord = {
-				attrib.texcoords[index.texcoord_index * 2 + 0],
-				attrib.texcoords[index.texcoord_index * 2 + 1]
-			};
-
-			auto idx = ui32(uniqueVertexCount);
-			for (uIndex i = 0; i < uniqueVertexCount; ++i)
-			{
-				if (vertex == outVertices[i])
-				{
-					idx = ui32(i);
-					break;
-				}
-			}
-			if (idx == uniqueVertexCount)
-			{
-				outVertices.push_back(vertex);
-				uniqueVertexCount++;
-			}
-			outIndices.push_back(idx);
-		}
-	}
-
-	return true;
-}
-
 std::shared_ptr<BuildAsset> BuildModel::create(std::shared_ptr<asset::Asset> asset)
 {
 	return std::make_shared<BuildModel>(asset);
@@ -107,17 +47,6 @@ asset::Model::Vertex convertVertex(aiMesh const* mesh, uIndex iVertex);
 std::vector<std::string> BuildModel::compile(logging::Logger &logger)
 {
 	std::vector<std::string> errors;
-	/*
-	auto bSuccess = loadObjModel(
-		asset->getAbsoluteSourcePath(),
-		[&logger, asset](logging::ECategory const& cate, std::string const& msg)
-		{
-			logger.log(cate, "[%s] %s", asset->getFileName().c_str(), msg.c_str());
-		},
-		this->mCompiledVertices, this->mCompiledIndices,
-		errors
-	);
-	//*/
 
 	auto modelPath = this->get<asset::Model>()->getAbsoluteSourcePath();
 	auto logPrefix = utility::formatStr("[BuildModel][%s]", modelPath.filename().string().c_str());
