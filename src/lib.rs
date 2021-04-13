d3883e04cd3c7b317578fcfbb7320b9004cb11b0
Dustin Yost <dustin.yost.t@gmail.com>
2021-04-13 11:54:17 -0400

    Wrap some SDL elements

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-use temportal_math::Vector;
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::error::Error;
+use std::time::Duration;
+use temportal_math::*;
 
 // Y-Up Right-Handed is +X, +Y, -Z
 pub fn global_right() -> Vector<f64, 3> {
@@ -9,4 +16,62 @@ pub fn global_up() -> Vector<f64, 3> {
 }
 pub fn global_forward() -> Vector<f64, 3> {
 	Vector::new([0.0, 0.0, -1.0])
+}
+
+struct EngineDisplay {
+	sdl: sdl2::Sdl,
+}
+
+impl EngineDisplay {
+
+	pub fn video_subsystem(&self) -> sdl2::VideoSubsystem {
+		self.sdl.video().unwrap()
+	}
+
+}
+
+struct Window {
+	window: sdl2::video::Window,
+}
+
+impl Window {
+
+	pub fn new(display: &EngineDisplay, title: &str, width: u32, height: u32) -> Window {
+		let mut builder = display.video_subsystem().window(title, width, height);
+		let window = builder.position_centered().build().unwrap();
+		Window { window }
+	}
+
+}
+
+pub fn run(_args: Vec<String>) -> Result<(), Box<dyn Error>> {
+	let display = EngineDisplay {
+		sdl: sdl2::init().unwrap(),
+	};
+
+	let window = Window::new(&display, "Demo1", 800, 600);
+	let mut canvas = window.window.into_canvas().build().unwrap();
+
+	canvas.set_draw_color(Color::RGB(50, 0, 50));
+	canvas.clear();
+	canvas.present();
+
+	// Game loop
+	let mut event_pump = display.sdl.event_pump().unwrap();
+	'gameloop: loop {
+		for event in event_pump.poll_iter() {
+			match event {
+				Event::Quit { .. } => break 'gameloop,
+				Event::KeyDown {
+					keycode: Some(Keycode::Escape),
+					..
+				} => break 'gameloop,
+				_ => {}
+			}
+		}
+		canvas.present();
+		::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+	}
+
+	Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,4 @@
-extern crate sdl2;
 
-use std::error::Error;
-use std::time::Duration;
-
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-
-fn main() -> Result<(), Box<dyn Error>> {
-	let sdl_context = sdl2::init().unwrap();
-	let video_subsystem = sdl_context.video().unwrap();
-
-	let window = video_subsystem
-		.window("rust-sdl2 demo", 800, 600)
-		.position_centered()
-		.build()
-		.unwrap();
-
-	let mut canvas = window.into_canvas().build().unwrap();
-
-	canvas.set_draw_color(Color::RGB(50, 0, 50));
-	canvas.clear();
-	canvas.present();
-
-	// Game loop
-	let mut event_pump = sdl_context.event_pump().unwrap();
-	'gameloop: loop {
-		for event in event_pump.poll_iter() {
-			match event {
-				Event::Quit { .. } => break 'gameloop,
-				Event::KeyDown {
-					keycode: Some(Keycode::Escape),
-					..
-				} => break 'gameloop,
-				_ => {}
-			}
-		}
-		canvas.present();
-		::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-	}
-
-	Ok(())
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	temportal_engine::run(std::env::args().collect())
 }
