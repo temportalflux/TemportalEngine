1d328a2ba15a29275b7f5515f4cb57739fc72cad
Dustin Yost <dustin.yost.t@gmail.com>
2021-04-12 20:04:03 -0400

    Add global world axes

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+
+// Y-Up Right-Handed is +X, +Y, -Z
+pub fn global_right() -> Vector<f64, 3> { Vector::new([ 1.0, 0.0, 0.0 ])}
+pub fn global_up() -> Vector<f64, 3> { Vector::new([ 0.0, 1.0, 0.0 ])}
+pub fn global_forward() -> Vector<f64, 3> { Vector::new([ 0.0, 0.0, -1.0 ])}
