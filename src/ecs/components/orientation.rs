9bfe1d99bb22e0db054d70241e716e8af3f8f9bd
Dustin Yost <dustin.yost.t@gmail.com>
2021-05-05 18:59:35 -0400

    Add a bunch of boids

--- src/ecs/components/orientation.rs
+++ src/ecs/components/orientation.rs
@@ -5,7 +5,7 @@ use crate::{
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Orientation(Quaternion);
+pub struct Orientation(pub Quaternion);
 
 impl Component for Orientation {
 	type Storage = VecStorage<Self>;
